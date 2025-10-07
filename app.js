51ef162c2ff51f70a1aceaf8ceb1cd6d3dfbb387
lilimarr <liliannwaedozie9@gmail.com>
2025-10-07 09:05:07 -0600

    easylink mobil nav

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
-let menuBtn = document.getElementById('menuBtn')
+// let menuBtn = document.getElementById('menuBtn')
 
-let menuStuff = document.getElementById('menuStuff')
+// let menuStuff = document.getElementById('menuStuff')
 
-menuBtn.addEventListener('click', () => {
-    menuStuff.classList.toggle("hidden")
-    // alert('hello')
-})
+// menuBtn.addEventListener('click', () => {
+//     menuStuff.classList.toggle("hidden")
+//     // alert('hello')
+// })
+
+
+ const menuBtn = document.getElementById('menuBtn');
+  const menuStuff = document.getElementById('menuStuff');
+
+  menuBtn.addEventListener('click', () => {
+    menuStuff.classList.toggle('hidden');
+  });
+
+  
